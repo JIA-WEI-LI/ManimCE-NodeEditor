a63a809592ad4c9df4c983f98361d333e8de2a3f
JIA-WEI-LI <magicsoldier19@gmail.com>
2025-07-04 16:59:28 +0800

    Corrected the undo and redo methods in the SceneHistory class to simplify the history recovery logic

--- node_editor/node_editor_window/core/scene_history.py
+++ node_editor/node_editor_window/core/scene_history.py
@@ -12,16 +12,37 @@ class SceneHistory():
     def undo(self):
         logger.debug("UNDO")
 
+        if self.history_current_step > 0:
+            self.history_current_step -= 1
+            self.restoreHistory()
+
     def redo(self):
         logger.debug("REDO")
 
+        if self.history_current_step + 1 < len(self.history_stack):
+            self.history_current_step += 1
+            self.restoreHistory()
+
     def restoreHistory(self):
-        logger.debug(f"Restoring history ... current step: {self.history_current_step:%d} {len(self.history_stack)}")
+        logger.debug(f"Restoring history ... current step: {self.history_current_step} {len(self.history_stack)}")
         self.restoreHistoryStamp(self.history_stack[self.history_current_step])
 
     def storeHistory(self, desc):
         logger.debug(f"Storing history ... current step: {self.history_current_step} {len(self.history_stack)}")
         hs = self.createHistoryStamp(desc)
+
+        # if the pointer (self.history_current_step) is not at the end of history stack
+        if self.history_current_step + 1 < len(self.history_stack):
+            self.history_stack = self.history_stack[0: self.history_current_step+1]
+
+        # history is outside of the limits
+        if self.history_current_step + 1 >= self.history_limit:
+            self.history_stack = self.history_stack[1:]
+            self.history_current_step -= 1
+
+        self.history_stack.append(hs)
+        self.history_current_step += 1
+        logger.debug(f"  -- setting step to: {self.history_current_step}")
 
     def createHistoryStamp(self, desc):
         return desc
